290b982e7dfcf7a28e950b767fe309d3fb8a240b
Hai Pham <swiften.svc@gmail.com>
2017-06-07 02:10:17 +0800

    Moved common components into XTestKitComponents

---
+++ src/main/java/org/swiften/javautilities/localizer/LocalizerContainerType.java
@@ -0,0 +1,21 @@
+package org.swiften.javautilities.localizer;
+
+import org.jetbrains.annotations.NotNull;
+import org.swiften.javautilities.localizer.LocalizerType;
+
+/**
+ * Created by haipham on 5/8/17.
+ */
+
+/**
+ * This interface provides {@link LocalizerType} instance for text
+ * localization.
+ */
+public interface LocalizerContainerType {
+    /**
+     * Get the associated {@link LocalizerType} instance.
+     * @return {@link LocalizerType} instance.
+     */
+    @NotNull
+    LocalizerType localizer();
+}
